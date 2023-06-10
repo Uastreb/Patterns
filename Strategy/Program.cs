7e9019ce9c852d6bfffe645ab654de3bee3d486b
Vadim <try.proger.vadim@gmail.com>
2023-06-10 13:40:51 +0300

    Добавил стратегию

---
+++ Strategy/Program.cs
@@ -0,0 +1,54 @@
+﻿using System.Numerics;
+
+namespace Strategy
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+
+    public interface IDiscriminantStrategy
+    {
+        double CalculateDiscriminant(double a, double b, double c);
+    }
+
+    public class OrdinaryDiscriminantStrategy : IDiscriminantStrategy
+    {
+        public double CalculateDiscriminant(double a, double b, double c)
+        {
+            return b * b - 4 * a * c;
+        }
+    }
+
+    public class RealDiscriminantStrategy : IDiscriminantStrategy
+    {
+        public double CalculateDiscriminant(double a, double b, double c)
+        {
+            double result = b * b - 4 * a * c;
+            return result >= 0 ? result : double.NaN;
+        }
+    }
+
+    public class QuadraticEquationSolver
+    {
+        private readonly IDiscriminantStrategy strategy;
+
+        public QuadraticEquationSolver(IDiscriminantStrategy strategy)
+        {
+            this.strategy = strategy;
+        }
+
+        public Tuple<Complex, Complex> Solve(double a, double b, double c)
+        {
+            var disc = new Complex(strategy.CalculateDiscriminant(a, b, c), 0);
+            var rootDisc = Complex.Sqrt(disc);
+            return Tuple.Create(
+              (-b + rootDisc) / (2 * a),
+              (-b - rootDisc) / (2 * a)
+            );
+        }
+    }
+}
